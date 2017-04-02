19fdbecd5f6dc84ca88db038b14035c85322b8db
tosFa <hoti.fatos@gmail.com>
2017-04-02 12:43:34 +0200

    remove unused code

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,8 @@
 import { reducer as form } from 'redux-form';
 import { combineReducers } from 'redux';
 import client from '../apollo/';
-import { reducer as auth } from './modules/auth';
 
 export default combineReducers({
   form,
   apollo: client.reducer(),
-  auth,
 });
